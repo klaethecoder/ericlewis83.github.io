5f4b9a1882b6b79272481b9b674ce5269f5fab50
ericlewis83 <ericlewis83@byui.edu>
2019-05-31 04:16:26 -0600

    update

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,4 +1,11 @@
-let todaysdate= new Date();
-
-document.getElementById
 ('current date').innerHTML=thedate;
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+var daysnames = days[d.getDay()];
+var monthsnames = months[d.getMonth()];
+
+document.getElementById("currentdate").innerHTML = daysnames + ", " + d.getDate() + " " + monthsnames + " " + d.getFullYear();
