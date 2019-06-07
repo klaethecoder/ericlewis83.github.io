ddf9d8d4f499f0afc6cbac747fe5586d5fb5a079
ericlewis83 <ericlewis83@byui.edu>
2019-06-07 16:06:36 -0600

    update

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,5 +1,5 @@
 var high =67;
-var tempaverage= document.getElementById("tempaverage").innerHTML;
+var tempaverage= document.getElementById("temp").innerHTML;
 var wSpeed = document.getElementById("windspeed").innerHTML;
 
 
