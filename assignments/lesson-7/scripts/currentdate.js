6c7fb401e7679b33293ee860cf427f239348864a
ericlewis83 <ericlewis83@byui.edu>
2019-06-08 12:32:04 -0600

    update

--- assignments/lesson-7/scripts/currentdate.js
+++ assignments/lesson-7/scripts/currentdate.js
@@ -1,5 +1,10 @@
 var d = new Date();
-var day = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-var month = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-document.getElementById('currentdate').innerHTML=(day[d.getDay()] + ", " + d.getDate() + " of " + month[d.getMonth()] + ", " + d.getFullYear());
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+var daysnames = days[d.getDay()];
+var monthsnames = months[d.getMonth()];
+
+document.getElementById("currentdate").innerHTML = daysnames + ", " + d.getDate() + " " + monthsnames + " " + d.getFullYear();
