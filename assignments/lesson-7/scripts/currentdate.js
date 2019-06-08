5e3e48cc948c917f78afd6f96f08057f217b4c46
ericlewis83 <ericlewis83@byui.edu>
2019-06-08 14:48:39 -0600

    update

--- assignments/lesson-7/scripts/currentdate.js
+++ assignments/lesson-7/scripts/currentdate.js
@@ -1,9 +1,6 @@
 var d = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-
 var daysnames = days[d.getDay()];
 var monthsnames = months[d.getMonth()];
 
