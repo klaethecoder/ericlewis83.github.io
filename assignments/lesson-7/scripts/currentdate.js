e776951052241efa5944ea855232c3e695803f3c
ericlewis83 <ericlewis83@byui.edu>
2019-06-08 07:50:13 -0600

    update

--- assignments/lesson-7/scripts/currentdate.js
+++ assignments/lesson-7/scripts/currentdate.js
@@ -1,10 +1,5 @@
 var d = new Date();
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var day = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var month = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+document.getElementById('currentdate').innerHTML=(day[d.getDay()] + ", " + d.getDate() + " of " + month[d.getMonth()] + ", " + d.getFullYear());
 
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-
-var daysnames = days[d.getDay()];
-var monthsnames = months[d.getMonth()];
-
-document.getElementById("currentdate").innerHTML = daysnames + ", " + d.getDate() + " " + monthsnames + " " + d.getFullYear();
--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,11 +1,10 @@
 var high =67;
-var tempaverage= document.getElementById("temp").innerHTML;
-var wSpeed = document.getElementById("windspeed").innerHTML;
+var tempaverage= document.getElementById('temp').innerHTML;
+var wSpeed = document.getElementById('windspeed').innerHTML;
 
 
 var windchill= 35.74 + 0.6215 * tempaverage - 35.75 * Math.pow(wSpeed, 0.16) + 0.4275 * tempaverage * Math.pow(wSpeed, 0.16);
 windchill= Math.round(windchill)  +  "&deg;F";
 
 
-document.getElementById ("windchill").innerHTML= windchill;
-
+document.getElementById ('windchill').innerHTML= windchill;
