4cb112c4c13754656fdcbeeb8713e4d0163b6444
ericlewis83 <ericlewis83@byui.edu>
2019-06-21 18:12:51 -0600

    update

--- assignments/lesson-9/scripts/town.js
+++ assignments/lesson-9/scripts/town.js
@@ -11,7 +11,7 @@ request.onload = function () {
 
     for (let i = 0; i < townArray.length; i++) {
         if (townArray[i].name == 'Fish Haven' || townArray[i].name == 'Preston' || townArray[i].name == 'Soda Springs') {
-            //name the variables
+         
             let section = document.createElement('section');
             let article = document.createElement('article');
             let h3 = document.createElement('h3');
@@ -21,14 +21,12 @@ request.onload = function () {
             let para4 = document.createElement('p');
             let image = document.createElement("IMG");
 
-            //Create the spot for the info
             h3.textContent = townArray[i].name;
             para1.textContent = townArray[i].motto;
             para2.textContent = "Year Founded:  " + townArray[i].yearFounded;
             para3.textContent = "Population:  " + townArray[i].currentPopulation;
             para4.textContent = "Average Rainfall:  " + townArray[i].averageRainfall + " inches";
 
-            //Give names for CSS
             section.setAttribute("class", "towninfo");
             h3.setAttribute("class", "townh3");
             para1.setAttribute("class", "townp1");
@@ -37,19 +35,16 @@ request.onload = function () {
             para4.setAttribute("class", "townp");
             image.setAttribute("class", "townimg");
 
-          //images to use based on town
             if (townArray[i].name == 'Fish Haven') {
                 image.setAttribute("src", "images/fishhaven.jpg");
-                image.setAttribute("alt", "lakeside homes");
+                image.setAttribute("alt", "Lakeside homes");
             } else if (townArray[i].name == 'Preston') {
                 image.setAttribute("src", "images/preston.jpg");
                 image.setAttribute("alt", "Preston store");
             } else if (townArray[i].name == 'Soda Springs') {
-                image.setAttribute("src", "images/sodasprings.jpg");
-                image.setAttribute("alt", "Geyser rainbow");
+                image.setAttribute("src", "Geyser rainbow");
             }
-          
-            //add info to HTML
+            
             article.appendChild(h3);
             article.appendChild(para1);
             article.appendChild(para2);
