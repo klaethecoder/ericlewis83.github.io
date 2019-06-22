f535bf071cb4af848c0a3c91aab70478d3ce1811
ericlewis83 <ericlewis83@byui.edu>
2019-06-21 18:36:33 -0600

    update

--- assignments/lesson-9/scripts/town.js
+++ assignments/lesson-9/scripts/town.js
@@ -31,8 +31,7 @@ request.onload = function () {
             h3.setAttribute("class", "townh3")
             para1.setAttribute("class", "townp1");
             para2.setAttribute("class", "townp");
-            para3.setAttribute("class", "townp");
-            para4.setAttribute("class", "townp");
+           
             
             
             if (townArray[i].name == 'Fish Haven') {
