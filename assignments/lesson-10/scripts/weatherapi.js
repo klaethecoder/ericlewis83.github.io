f761acfe40e5db6c352da8c8763ec65f213f4d59
ericlewis83 <ericlewis83@byui.edu>
2019-06-24 07:21:49 -0600

    update

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,17 @@
+let weatherRequest=new XMLHttpRequest();
+let apiURLstring= 'https://api.openweathermap.org/data/2.5/weather?id=5604473&unitimperial&APPIDa79b147b7791f4c09f3c781da76d6580';
+
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload=function() {
+letweatherData=JSON.parse(weatherRequest.responseText);
+console.log(weatherData)};
+
+document.getElementById('cc-temp').innerHTML=weatherData.main.temp;
+
+let icon="http://openweathermap.org/img/w"+10n 'WeatherData.Weather[0].icon,'+".png"
+let desc=weatherData.weather.description;
+
+document.getElementByID('cc-img').setAttribute('src', icon);
+document.getElementByID('cc-img').setAttribute('alt', desc);
