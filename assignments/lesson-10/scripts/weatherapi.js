ba2afff481bcb2831f7d187237ed28713a263c49
ericlewis83 <ericlewis83@byui.edu>
2019-06-25 11:56:07 -0600

    updat

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -7,12 +7,13 @@ weatherRequest.send();
 weatherRequest.onload = function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData)
+
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+
+    let icon = 'http://openweathermap.org/img/w/' + weatherData.Weather[0].icon + '.png'
+    let desc = weatherData.weather[0].description;
+
+    document.getElementById('cc-img').setAttribute('src', icon);
+    document.getElementById('cc-img').setAttribute('alt', desc);
 };
-
-document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
-
-let icon = 'http://openweathermap.org/img/w/' + 'weatherData.Weather[0].icon' + '.png'
-let desc = weatherData.weather[0].description;
-
-document.getElementById('cc-img').setAttribute('src', icon);
-document.getElementById('cc-img').setAttribute('alt', desc);
