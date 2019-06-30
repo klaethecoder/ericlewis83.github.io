73287553a4abc0b93e5f2da5ac5e37b368d62e92
ericlewis83 <ericlewis83@byui.edu>
2019-06-29 22:02:25 -0600

    update

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,5 +1,5 @@
- let t = parseInt(document.getElementById('hitemp').innerHTML);
- let s = parseInt(document.getElementById('windspeed').innerHTML);
+ let t = parseFloat(document.getElementById('hitemp').innerHTML);
+ let s = parseFloat(document.getElementById('windspeed').innerHTML);
  let result = 35.74 + (0.6215 * t) - (35.75 * s ** 0.16) + (0.4275 * t * s ** 0.16);
  document.getElementById('windchill').innerHTML = result.toFixed(1);
 
