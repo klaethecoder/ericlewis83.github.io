7721c752c5deae2696425f313ff0f40ffc97f946
ericlewis83 <ericlewis83@byui.edu>
2019-07-12 09:03:32 -0600

    a

---
+++ assignments/templeinnproject/scripts/weathersum.js
@@ -0,0 +1,50 @@
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&APPID=a79b147b7791f4c09f3c781da76d6580";
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    document.getElementById("hitemp").innerHTML = weatherData.main.temp_max;
+    document.getElementById("currently").innerHTML = weatherData.weather[0].main;
+    document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
+    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+}
+
+
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5596475&units=imperial&APPID=a79b147b7791f4c09f3c781da76d6580";
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    document.getElementById("hitemp").innerHTML = weatherData.main.temp_max;
+    document.getElementById("currently").innerHTML = weatherData.weather[0].main;
+    document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
+    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+}
+
+
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5780026&units=imperial&APPID=a79b147b7791f4c09f3c781da76d6580";
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    document.getElementById("hitemp").innerHTML = weatherData.main.temp_max;
+    document.getElementById("currently").innerHTML = weatherData.weather[0].main;
+    document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
+    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+}
+
+
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5856194&units=imperial&APPID=a79b147b7791f4c09f3c781da76d6580";
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    document.getElementById("hitemp").innerHTML = weatherData.main.temp_max;
+    document.getElementById("currently").innerHTML = weatherData.weather[0].main;
+    document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
+    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+}
