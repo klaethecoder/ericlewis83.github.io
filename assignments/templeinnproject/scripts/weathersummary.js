f44b1ccd7773705f25d3eb90a9108089ec7db2d2
ericlewis83 <ericlewis83@byui.edu>
2019-07-12 08:51:54 -0600

    a

--- assignments/templeinnproject/scripts/weathersummary.js
+++ assignments/templeinnproject/scripts/weathersummary.js
@@ -1,4 +1,4 @@
-let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&APPID=a79b147b7791f4c09f3c781da76d6580";
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&APPID=3a18989acfc61f78f073dcff5a24e90c";
 let weatherRequest = new XMLHttpRequest();
 weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.send();
@@ -48,16 +48,3 @@ weatherRequest.onload = function () {
     document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
     document.getElementById("humidity").innerHTML = weatherData.main.humidity;
 }
-
-
-
-/*Calculate windchill*/
-let t = parseFloat(document.getElementById('hitemp').innerHTML);
-let s = parseFloat(document.getElementById('windspeed').innerHTML);
-let result = 35.74 + (0.6215 * t) - (35.75 * s ** 0.16) + (0.4275 * t * s ** 0.16);
-document.getElementById('windchill').innerHTML = result.toFixed(1);
-if (t <= 50 && s >= 3) {
-    let windchill = Math.round(n * 100) / 100;
-    document.getElementById('windchill').innerHTML = windchill;
-} else
-    document.getElementById('windchill').innerHTML = "N/A";
