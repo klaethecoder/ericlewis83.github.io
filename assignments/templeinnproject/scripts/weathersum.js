d9c71dce96807330433c5bb25c8d0a0248e48229
Ryan Richard <ryanrichard@Ryans-MacBook-Pro.local>
2019-07-15 06:09:59 -0600

    Created the JSON file, got it synced up with the temples.html file and made sure that all the data that is being pulled from the templeinfo and weathersum js files are working properly.

--- assignments/templeinnproject/scripts/weathersum.js
+++ assignments/templeinnproject/scripts/weathersum.js
@@ -4,12 +4,11 @@ weatherRequest1.open('GET', apiURLstring1, true);
 weatherRequest1.send();
 weatherRequest1.onload = function () {
     let weatherData = JSON.parse(weatherRequest1.responseText);
-    console.log(document.getElementById("boise-currently"))
-    console.log(weatherData)
     document.getElementById("boise-hitemp").innerHTML = weatherData.main.temp_max;
     document.getElementById("boise-currently").innerHTML = weatherData.weather[0].main;
     document.getElementById("boise-windSpeed").innerHTML = weatherData.wind.speed;
     document.getElementById("boise-humidity").innerHTML = weatherData.main.humidity;
+    document.getElementById("boise-windchill").innerHTML = getWindChill("boise");
 }
 
 
@@ -19,12 +18,11 @@ weatherRequest2.open('GET', apiURLstring2, true);
 weatherRequest2.send();
 weatherRequest2.onload = function () {
     let weatherData = JSON.parse(weatherRequest2.responseText);
-    console.log(document.getElementById("hitemp"))
-    console.log(weatherData)
     document.getElementById("if-hitemp").innerHTML = weatherData.main.temp_max;
     document.getElementById("if-currently").innerHTML = weatherData.weather[0].main;
     document.getElementById("if-windSpeed").innerHTML = weatherData.wind.speed;
     document.getElementById("if-humidity").innerHTML = weatherData.main.humidity;
+    getWindChill("if")
 }
 
 
@@ -34,12 +32,11 @@ weatherRequest3.open('GET', apiURLstring3, true);
 weatherRequest3.send();
 weatherRequest3.onload = function () {
     let weatherData = JSON.parse(weatherRequest3.responseText);
-    console.log(document.getElementById("hitemp"))
-    console.log(weatherData)
     document.getElementById("provo-hitemp").innerHTML = weatherData.main.temp_max;
     document.getElementById("provo-currently").innerHTML = weatherData.weather[0].main;
     document.getElementById("provo-windSpeed").innerHTML = weatherData.wind.speed;
     document.getElementById("provo-humidity").innerHTML = weatherData.main.humidity;
+    getWindChill("provo")
 }
 
 
@@ -49,29 +46,24 @@ weatherRequest4.open('GET', apiURLstring4, true);
 weatherRequest4.send();
 weatherRequest4.onload = function () {
     let weatherData = JSON.parse(weatherRequest4.responseText);
-    console.log(document.getElementById("hitemp"))
-    console.log(weatherData)
     document.getElementById("slc-hitemp").innerHTML = weatherData.main.temp_max;
     document.getElementById("slc-currently").innerHTML = weatherData.weather[0].main;
     document.getElementById("slc-windSpeed").innerHTML = weatherData.wind.speed;
     document.getElementById("slc-humidity").innerHTML = weatherData.main.humidity;
+    getWindChill("slc")
 }
 
-/*Calculate windchill */
-let cities = ["boise-", "if-", "provo-", "slc-"];
-for(let i = 0; i< cities.length; i++){
-    let cityTemp = document.getElementById(`${cities[i]}hitemp`).innerHTML;
-    let cityWind = document.getElementById(`${cities[i]}windSpeed`).innerHTML;
-    console.log(cityTemp)
-    let t = Number(cityTemp);
-    let s = Number(cityWind);
-    let result = 35.74 + (0.6215 * t) - (35.75 * s ** 0.16) + (0.4275 * t * s ** 0.16);
-    // let windchill = document.getElementById(`${cities[i]}windchill`).innerHTML = result.toFixed(1);
-    // console.log(`number ${i}: ${t} , ${s}`)
-    console.log(t, s, result)
-    if (t <= 50 && s >= 3) {
-        let windchill = Math.round(n * 100) / 100;
-        document.getElementById(`${cities[i]}windchill`).innerHTML = windchill;
-    } else
-        document.getElementById(`${cities[i]}windchill`).innerHTML = "N/A";
-}
+/*Calculates windchill */
+
+function getWindChill(city) {
+    let t= parseInt(document.getElementById(`${city}-hitemp`).innerHTML);
+    let s = parseInt(document.getElementById(`${city}-windSpeed`).innerHTML);
+    result = windChill(t, s);
+    return document.getElementById(`${city}-windchill`).innerHTML = result;
+  }
+  
+  function windChill(t, s) {
+    let windChillResult = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    let windchill = windChillResult.toFixed(2);
+    return windchill;
+  }
